af7cd2566ac7eb2f935baa46cd69a8a47da52669
kumarshahial-pixel <kumarshahial@gmail.com>
2025-11-06 21:26:33 +0530

    Update PartA_DI.java

--- PartA_DI.java
+++ PartA_DI.java
@@ -1,1 +1,24 @@
+class DatabaseService {
+    public void connect() {
+        System.out.println("Database connected successfully!");
+    }
+}
 
+class UserService {
+    private DatabaseService dbService;
+    public UserService(DatabaseService dbService) {
+        this.dbService = dbService;
+    }
+    public void registerUser(String name) {
+        dbService.connect();
+        System.out.println("User registered: " + name);
+    }
+}
+
+public class PartA_DI {
+    public static void main(String[] args) {
+        DatabaseService db = new DatabaseService();
+        UserService userService = new UserService(db);
+        userService.registerUser("Sumit");
+    }
+}
